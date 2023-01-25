1b4948dcd81f318dfb2cc0a28b85c7f8fb61f857
lark <ivaxen2000@gmail.com>
2023-01-25 18:47:21 +0200

    del test init

--- pars_skript/position/test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = 'https://routing.openstreetmap.de/routed-car/route/v1/driving/20.5031,54.71529;20.50416,54.7147?overview=false&geometries=polyline&steps=true'
-
-response = requests.get(url)
-
-print(response.json())
