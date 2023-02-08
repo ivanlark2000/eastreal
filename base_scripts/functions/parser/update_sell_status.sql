28e39e2777e77f46ebb0c1a83d828aa8c5571f98
lark <ivaxen2000@gmail.com>
2023-02-08 21:44:22 +0200

    add update sell status

---
+++ base_scripts/functions/parser/update_sell_status.sql
@@ -0,0 +1,27 @@
+-------------------------------------------------------------------
+--Функция которая обновляет статусы в историчность в БД
+--CREATE DATE 2023.02.08 
+
+CREATE OR REPLACE FUNCTION update_sell_status(
+    city_id smallint,
+    VARIADIC sitids bigint[]
+)
+RETURNS void 
+AS
+$BODY$
+
+UPDATE inf_sys 
+SET f_sell_status = 2
+WHERE f_city = city_id AND site_id NOT IN (
+    SELECT * FROM unnest($2)
+);
+
+UPDATE inf_miss_ads
+SET f_sell_status = 2
+WHERE f_city = city_id AND site_id NOT IN (
+    SELECT * FROM unnest($2)
+);
+
+$BODY$
+LANGUAGE SQL;
+
