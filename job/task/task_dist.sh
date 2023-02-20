a6121e22a07b8cba3bcee85e2c457dafd1413421
lark2000 <ivaxen2000@gmail.com>
2023-02-20 22:22:57 +0300

    cor djob dist

--- job/task/task_dist.sh
+++ job/task/task_dist.sh
@@ -1,16 +1,19 @@
+#!/bin/zsh
 #Джоб по запуску закачки дистанции пешком и на машине 
 #CREATE DATE 2023.01.29
 dir_project=/home/lark/project/eastreal
-dir=${dir_project}/job/log/task_dist.log;
 name_db=eastreal;
 
-echo `date` >> $dir;
+echo '-----------------------------------------'
+echo `date`;
 
 if  [ -z "$port" ] ;
     then 
 
-        . $dir_project/pars_script/settings/.env; 
+        . $dir_project/settings/.env; 
 fi;
-psql -c '\x' -c "CALL start_task_dist();" --dbname=$name_db >> $dir 2>&1;
+psql -c '\x' -c "CALL start_task_dist();" --dbname=$name_db;
 
-echo `date` >> $dir;
+echo `date`;
+
+echo '-----------------------------------------'
